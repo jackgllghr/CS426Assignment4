029991a1dbdb8d7d16905bd7883e2704d1ea293a
Jack Gallagher <jackgllghr@gmail.com>
2014-04-23 02:20:40 +0100

    Added a pendulum swinging with the attractor graphed

--- CS426Assignment4/CS426Assignment4.cpp
+++ CS426Assignment4/CS426Assignment4.cpp
@@ -1,173 +1,198 @@
-// C.Markham Feb 2010
-
-// FreeGlut Version of code created in lecture (with an extra 'paddle')
-// You could create your own binaries from the freeglut VS2008 project supplied from the download site.
-// http://freeglut.sourceforge.net/
-// In this example the "Solution" contains both our code and binaries created from the above and copied into this project.
-// The binaries are the include files, library files and dynamic linked libaries from the freeglut code (without the project files or sourcecode).
-// Select Build - Clean Solution - Build - Rebuild Solution and then click on Start Debugging (run) and it should work.
-// You may need to right click on First_FreeGlut on Solution explorer (see panel to left of this) and select Properties (last on list)
-// Then choose the Configuration (Top left) All Configuarions then
-// Configuration Properties - C\C++ - General - Additional Include Directories then
-// either edit the path to the following or double click in the white space to the right of the "Additional Include Directories" 
-// to create a new directory that points to the binaries the path will be something like the following but not the same
-// "C:\Documents and Settings\ ...specific to your machine ...\My Documents\Visual Studio 2008\Projects\First_FreeGlut\freeglut_bin"
-// Repeat the procedure for the Linker so the complier can find the freeglut.lib file in the same directory
-// Configuration Properties - Linker - General - Additional Library Directories (a bit further down the list)
-// Finally put freeglut.dll in the runtime directory, to cover both Release and Debug modes you will need to do this twice (Debug will do)
-// C:\Documents and Settings\...specific to your machine...\My Documents\Visual Studio 2008\Projects\First_FreeGlut\Release
-// C:\Documents and Settings\...specific to your machine...\My Documents\Visual Studio 2008\Projects\First_FreeGlut\Debug
-// If all else fails you could try double clicking on First_FreeGlut in the release directory and see it running.
-// VS2010 Right click on First_FreeGlut in Solution explorer (then VC++ directories) and add freeglut library to both Include Linker directories
-
+﻿//CS426 Assignment 4, Jack Gallagher 69537058
+//Press u or d to alter the velocity of the pendulum, and l or r to alter the angle
 #include "stdafx.h"
-
 #include <freeglut.h>
+#include <math.h>
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#define NUM 50000
+#define FRAMETIMEMS 5  // One frame every five milliseconds
+#define PI 3.142
 
-// Black box moving left/right position
-float xb = 200.0f;
-float v = 0.0f;
-float t = 0.0f;
-float dt = 0.10f;
+void circle(float, float, float);
+void draw_bob(float);
+void gl_pnum(float, float, float);
+void gl_pstring(float, float, char *);
 
-float yb = -160.0f;
+// Declare variables for pendulum globally after main() 
+float x[NUM], y[NUM];
+float g = 9.81, l = 0.25, D=-0.5, dt = 0.0002;
+float v = 15.1, a = 0, t = 0, b = 0, c = 0.0;
+int i = 0;
 
-// Blue paddle moving up/down
-int yp = 0;
+int window1 = 0;
 
-/* GLUT callback Handlers */
-static void display(void)
+//Draw the pendulum
+void draw_bob(float theta){
+	float offset = PI / 2.0f;
+	glColor3f(1.0, 0, 0);
+
+	// Draw line from center to bob 
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(0.5f + (0.3f*cos(theta-offset)), 0.5f + (0.3f*sin(theta-offset)));
+	glEnd();
+	circle(0.5f + (0.3f*cos(theta-offset)), 0.5f + (0.3f*sin(theta-offset)), 0.05f);
+}
+// Draw circle 
+void circle(float x, float y, float r){
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y);
+	for (int n= 0; n <= 24; n++)
+	{
+		float const t = 2 * 3.14159*(float)n / 24.0f;
+		glVertex2f(x + sin(t)*r, y + cos(t)*r);
+	}
+	glEnd();
+}
+// Plot attractor in x[],y[],num 
+void plot_attractor(float x[], float y[], int n)
 {
-	// Clear screen
+	float minx, maxx, miny, maxy;
+	maxx = minx = x[0];
+	maxy = miny = y[0];
+	for (int j = 1; j < n; j++)
+	{
+		if (maxx<x[j])maxx = x[j];
+		if (minx>x[j])minx = x[j];
+		if (maxy<y[j])maxy = y[j];
+		if (miny>y[j])miny = y[j];
+	}
+
+	gl_pnum(0, -0.08, minx);
+	gl_pnum(0.9, -0.08, maxx);
+	gl_pstring(0.4, -0.12, "Angle");
+	gl_pnum(-0.24, 0, miny);
+	gl_pnum(-0.24, 0.95, maxy);
+	gl_pstring(-0.25, 0.5, "Velocity");
+
+	gl_pstring(-0.1, -0.17, "Velocity=");
+	gl_pnum(0.2, -0.17, v);
+
+	gl_pstring(0.6, -0.17, "Angle=");
+	gl_pnum(0.85, -0.17, a);
+
+
+
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(-0.01, -0.01);
+	glVertex2f(-0.01, 1.01);
+	glVertex2f(1.01, 1.01);
+	glVertex2f(1.01, -0.01);
+	glEnd();
+
+	glPointSize(2);
+	glBegin(GL_POINTS);
+	glColor3f(1.0, 0.0, 0.0);
+	for (int j = 0; j < n; j++){
+		glVertex2f((x[j]-minx) / (maxx-minx), (y[j]-miny) / (maxy-miny));
+	}
+	glEnd();
+}
+// Print the value of a variable on screen 
+void gl_pnum(float x, float y, float n){
+	char text[20];
+	sprintf_s(text, "%3.2f", n);
+	gl_pstring(x, y, text);
+}
+
+// Print a string on screen 
+void gl_pstring(float x, float y, char text[]){
+	glPushMatrix();
+	glTranslatef(x, y, 1);
+	glScalef(0.0005, 0.0005, 1);
+	glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char *)text);
+	glPopMatrix();
+}
+
+static void Draw(void){
+	// Draw ball motion on left of screen
+	glViewport(0, 0, 400, 400);
+
+	// Make the following region active;
+	glScissor(0, 0, 400, 400);
+	glEnable(GL_SCISSOR_TEST);
+
+	glClearColor(0, 0, 0, 1);
+
+	// Clear left side
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// Set drawing colour to black (red, green, blue)
-	glColor3f(0.0, 0.0, 0.0);
-	// Draw a 20x20 rectangle centered on (x,0)
-	glRectf(240 + yb, 240 + xb, 272 + yb, 272 + xb);
+	// Set drawing colour to red (red, green, blue)
+	glColor3f(1.0, 0.0, 0.0);
 
-	// Set drawing colour to black (red, green, blue)
-	glColor3f(0.0, 0.0, 1.0);
+	//Draw the pendulum with angle a
+	draw_bob(a);
 
-	// Draw a 20x20 rectangle centered on (x,0)
-	glRecti(246, 210 + yp, 262, 312 + yp);
+	// Set screen back to normal
+	glScissor(400, 0, 800, 400);
+	
 
+	glColor3f(0.0, 1.0, 0.0);
+	
+	glViewport(400, 0, 400, 400);
+	
+	glClearColor(1, 1, 1, 1);
+	//Clear right side
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	glLineWidth(4.0);
+
+	plot_attractor(x, y, i);
+	glDisable(GL_SCISSOR_TEST);
+
+	// Send message to cause re-draw (to return to Draw)
+	glutPostRedisplay();
 	glutSwapBuffers();
 }
-
-
-// Callback function to allow dynamic resizing of runtime display
-static void resize(int width, int height)
+static void Update(int dummy)
 {
-	glViewport(0, 0, width, height);
-	glLoadIdentity();
-	glMatrixMode(GL_PROJECTION);
+	// Advance pendulum using Euler method 
+	x[i] = a;
+	y[i] = v;
+	a = a + (v*dt);
+	v = v + ((((-g / l)*sin(a)) + (D*v) + (c*fabs(sin(b*t))))*dt);
+	t = t + dt;
+	if (i<NUM) i++;
 
-	// Maps 512x512 vertex co-ords 1:1 to the screen co-ords 
-	// Rare in practice that you would do this
-	gluOrtho2D(0.0, 512.0, 0.0, 512.0);
+	glutTimerFunc(FRAMETIMEMS, Update, 0); 
 }
-
-
-// Callback to manage standard keys on the keyboard
-// In this case 'u' moves paddle up and 'd' moves paddle down
+//Called when a key is pressed
 static void key(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-	case 'u': yp += 5; break;
-	case 'd': yp -= 5; break;
-
+	case 'u': v += 5; break;
+	case 'd': v -= 5; break;
+	case 'l': a += 1; break;
+	case 'r': a -= 1; break;
 	case 'Q':
-	case 'q': glutLeaveMainLoop(); break;  // (Stop!)
-
+	case 'q': glutLeaveMainLoop(); break;
 	default: break;
 	}
-
 	glutPostRedisplay();
 }
+// Program starts here
+int main(int argc, char * argv[]){
+	glutInitWindowPosition(100, 50);
+	glutInitWindowSize(800, 400);
 
-// Callback to cursor keys on the keyboard
-// In this case Up-Arrow moves paddle up and Down-Arrow moves paddle down
-
-static void special(int key, int x, int y)
-{
-	switch (key)
-	{
-	case GLUT_KEY_UP:        yp += 5;        break;
-	case GLUT_KEY_DOWN:      yp -= 5;        break;
-
-	default: break;
-	}
-}
-
-
-// Runs during idle time
-// Double buffer this is the frame rate of the machine typically 50/60Hz
-// Single buffer this is very much faster
-static void idle(void)
-{
-	// Simulate the falling motion of the ball
-	// Second order differential equation written as two first order
-	// Evaluated with Euler/Cromer method
-	xb = xb + (v*dt);
-	v = v - (1.0f)*xb*dt;
-	// Bounce added
-	if (xb<0.0f) { v = -0.7f*v; xb = 0.0f; }
-
-	t = t + dt;
-
-	// Move horizonatal at fixed speed
-	yb = yb + 0.3f;
-
-	// Tell OpenGL to redraw screen now you have updated the 'game play'
-	glutPostRedisplay();
-}
-
-
-/* Program entry point - starts here */
-int main(int argc, char *argv[])
-{
-	// Create a Window called "FreeGlut One" at 100,100 on main screen, sized 512x512 pixels
-	glutInitWindowSize(512, 512);
-	glutInitWindowPosition(100, 100);
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
-	glutCreateWindow("FreeGLUT One");
 
-	// 2D program use an Orthographic projection to keep everything independent of Z
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+	window1 = glutCreateWindow("CS426 Assignment 4 - Pendulum and Attractor");
+
+	// Set clearscreen colour to white
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
 	glMatrixMode(GL_PROJECTION);
-	// Maps 512x512 vertex co-ords 1:1 to the screen co-ords 
-	// Rare in practice that you would do this
-	gluOrtho2D(0.0, 512.0, 0.0, 512.0);
-
-
-	// Set screen to yellow
-	glClearColor(1, 1, 0, 1);  // Clearscree(Red, Green, Blue)
-
-	// Load location of ReShapeFunction into mainloop so it can be called if required 
-	// "resize" is both the name of the function and when referenced the memory address of the function 
-	glutReshapeFunc(resize);
-
-	// Callback function used when mainloop decides/graphics card to re-draw the scene
-	// Sending a glutPostRedisplay(); message to mainloop will cause display to be called
-	glutDisplayFunc(display);
-
-	// Callback functions to manage keyboard input
+	
+	gluOrtho2D(-0.3, 1.1, -0.2, 1.1);
+	
+	glutDisplayFunc(Draw);
+	glutTimerFunc(FRAMETIMEMS, Update, 0); 
 	glutKeyboardFunc(key);
-	glutSpecialFunc(special);
-
-	// Called during idle time (like Update in XNA)
-	// Doublebuffer at frame rate
-	glutIdleFunc(idle);
-
-	// Start mainloop running (go!)
 	glutMainLoop();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
